712bff254895cc57eef77e9d43f8e7fb2850559f
Ninjavin <vinjain2006@gmail.com>
2024-06-28 23:34:17 +0530

    setup eslint and prettier & husky

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "react-app",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    // Additional rules can be added here
+  },
+};
